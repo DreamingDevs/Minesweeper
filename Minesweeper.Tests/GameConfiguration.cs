71928182c81a0c1d07dcb01fc8006a76276c629a
Venkata Raghavaiah Rami Vemula <rami.ramilu@gmail.com>
2019-05-25 13:27:13 +0530

    Unit Test Cases.

---
+++ Minesweeper.Tests/GameConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Minesweeper.Contracts;
+using Minesweeper.Core;
+using Minesweeper.Core.Handlers;
+using Minesweeper.Model;
+using System.Collections.Generic;
+namespace Minesweeper.Tests
+{
+    public class GameConfiguration
+    {
+        public static IGame CreateGame(int height, int width, Difficulty level)
+        {
+            var randomMiner = new RandomMiner();
+            var validator = new Validator();
+            var handlers = LoadHandlers();
+            var game = new Game(randomMiner, validator, handlers);
+            game.CreateBoard(height, width, level);
+            return game;
+        }
+        
+        private static IBlockActionHandlers LoadHandlers()
+        {
+            var handlers = new BlockActionHandlers
+            {
+                Handlers = new Dictionary<BlockAction, IActionHandler>()
+                {
+                    { BlockAction.Flag, new FlagActionHandler() },
+                    { BlockAction.Open, new OpenActionHandler() }
+                }
+            };
+            return handlers;
+        }
+    }
+}
---
+++ Minesweeper.Tests/GameTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Minesweeper.Contracts;
+
+namespace Minesweeper.Tests
+{
+    [TestClass]
+    public class GameTests
+    {
+        public GameTests()
+        {
+        }
+
+        [TestMethod]
+        public void GameWithMines()
+        {
+            var game = GameConfiguration.CreateGame(3, 3, Model.Difficulty.Low);
+            Assert.IsTrue(game.CurrentBoard.FlattenBlocks.Count(p => p.BlockType == Model.BlockType.Mine) > 0);
+        }
+
+        [TestMethod]
+        public void OpenNormalBlockSuccessTest()
+        {
+            var game = GameConfiguration.CreateGame(3, 3, Model.Difficulty.Low);
+            var normalBlock = game.CurrentBoard.FlattenBlocks.FirstOrDefault(p => p.BlockType == Model.BlockType.Normal && p.BlockState == Model.BlockState.Unopened);
+            game.ExecuteAction(normalBlock.Position, Model.BlockAction.Open);
+            Assert.AreEqual(Model.BlockState.Opened, game.CurrentBoard.Blocks[normalBlock.Position.X, normalBlock.Position.Y].BlockState);
+            Assert.IsTrue(!game.CurrentBoard.ToString().Contains("X"));
+        }
+
+        [TestMethod]
+        public void OpenMineBlockFailTest()
+        {
+            var game = GameConfiguration.CreateGame(3, 3, Model.Difficulty.Low);
+            var totalMines = game.CurrentBoard.FlattenBlocks.Count(p => p.BlockType == Model.BlockType.Mine);
+
+            var normalBlock = game.CurrentBoard.FlattenBlocks.FirstOrDefault(p => p.BlockType == Model.BlockType.Mine && p.BlockState == Model.BlockState.Unopened);
+            game.ExecuteAction(normalBlock.Position, Model.BlockAction.Open);
+
+            Assert.AreEqual(Model.BlockState.Errored, game.CurrentBoard.Blocks[normalBlock.Position.X, normalBlock.Position.Y].BlockState);
+            Assert.AreEqual(true, game.IsCompleted);
+            Assert.AreEqual(totalMines - 1, game.CurrentBoard.FlattenBlocks.Count(p => p.BlockType == Model.BlockType.Mine && p.BlockState == Model.BlockState.Opened));
+            Assert.IsTrue(game.CurrentBoard.ToString().Contains("X"));
+        }
+
+        [TestMethod]
+        public void FlagBlockTest()
+        {
+            var game = GameConfiguration.CreateGame(3, 3, Model.Difficulty.Low);
+            var normalBlock = game.CurrentBoard.FlattenBlocks.FirstOrDefault(p => p.BlockType == Model.BlockType.Mine && p.BlockState == Model.BlockState.Unopened);
+            game.ExecuteAction(normalBlock.Position, Model.BlockAction.Flag);
+            Assert.AreEqual(Model.BlockState.Flagged, game.CurrentBoard.Blocks[normalBlock.Position.X, normalBlock.Position.Y].BlockState);
+            Assert.IsTrue(game.CurrentBoard.ToString().Contains("?"));
+        }
+
+
+    }
+}
